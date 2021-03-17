ea29a85c3c800698dcc8c83bb1276742a8aa8ead
jorgenholme96 <jorgen.holme@bouvet.no>
2021-03-17 09:49:03 +0100

    Add plantSelector component

---
+++ EchoFramework/src/components/plantSelector/plantSelector.tsx
@@ -0,0 +1,67 @@
+import { Dropdown } from '@equinor/echo-components';
+import { EchoEvents, eventHub, Plant, usePlants, usePlantSettings } from '@equinor/echo-core';
+import { StarProgress } from '@equinor/eds-core-react';
+import React, { useState } from 'react';
+
+interface PlantSelectorProps {
+    styleClass?: 'compact' | 'default';
+    isDisabled?: boolean;
+}
+
+const PlantSelector: React.FC<PlantSelectorProps> = ({ styleClass, isDisabled }: PlantSelectorProps) => {
+    const plants = usePlants();
+    const { instCode: selectedPlantCode, plantName: selectedPlant } = usePlantSettings();
+    const dropdownData = plants.map((plant: Plant) => plant.description);
+    const [acquiredPlantName, setAcquiredPlantName] = useState('');
+
+    const isPlantsLoading = plants.length === 0;
+
+    if (!selectedPlant && selectedPlantCode && plants && !acquiredPlantName) {
+        const newName = plants.find((plant: Plant) => plant.instCode === selectedPlantCode)?.description;
+        if (newName) {
+            setAcquiredPlantName(newName);
+        }
+    }
+
+    const selectedPlantName = selectedPlant ? selectedPlant : acquiredPlantName;
+
+    const handlePlantSelected = async (text: string): Promise<void> => {
+        eventHub.emit(EchoEvents.ChangePlant, { text, plants });
+    };
+
+    const filterPlants = (data: string[], filter: string): string[] => {
+        const filteredPlants = plants.filter(
+            (item) =>
+                item.description.toLowerCase().indexOf(filter.trim().toLowerCase()) > -1 ||
+                item.instCode.toLowerCase().indexOf(filter.trim().toLowerCase()) > -1
+        );
+
+        return filteredPlants.map((plant: Plant) => plant.description);
+    };
+
+    if (isPlantsLoading) {
+        return (
+            <div style={{ display: 'flex', alignItems: 'flex-end' }}>
+                <span style={{ textAlign: 'center', fontSize: '24px', marginRight: '0.5rem' }}>Loading plants</span>
+                <StarProgress size={32} />
+            </div>
+        );
+    }
+
+    return (
+        <Dropdown
+            showSearch={true}
+            selected={selectedPlantName}
+            data={dropdownData}
+            filterFunc={filterPlants}
+            openDownWards={true}
+            placeholder="Select plant"
+            setSelected={handlePlantSelected}
+            isDisabled={isDisabled || !navigator.onLine}
+            disabledText="Disabled while syncing or loading data"
+            styleClass={styleClass ? styleClass : 'default'}
+        />
+    );
+};
+
+export default PlantSelector;
