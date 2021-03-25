e859d321ab77d84254809adabee86a18a03900c0
vft-equinor <vft@equinor.com>
2021-03-25 11:42:10 +0100

    Enhanced Error component.

--- EchoFramework/src/components/errorBoundary/error.tsx
+++ EchoFramework/src/components/errorBoundary/error.tsx
@@ -1,11 +1,20 @@
+import { Button, Icon } from '@equinor/eds-core-react';
+import { error_filled } from '@equinor/eds-icons';
 import React from 'react';
+import { themeConst } from '../../theme/themeConst';
+import style from './error.module.css';
+
+Icon.add({ error_filled });
 
 const Error: React.FC = () => {
     return (
-        <div>
-            <h2>Something went wrong.</h2>
+        <div className={style.container}>
+            <Icon className={style.icon} color={themeConst.darkEquiRed} name={'error_filled'} />
+            <h1>Hmm, something went wrong..</h1>
+            <p>An unexpected error has occurred, please try again.</p>
+            <Button variant="outlined">Read more</Button>
         </div>
     );
-}
+};
 
 export default Error;
