cbd048626421d4b6d27ae2ee8b039d8e00c217ff
Christopher Berge Hove <chris@nogglings.com>
2021-03-25 15:51:43 +0100

    added echo client

---
+++ EchoClient/src/index.tsx
@@ -0,0 +1,27 @@
+import EchoCore, { PanelHandler } from "@equinor/echo-core";
+import { EchoContent, mainMenu, searchPanel } from "@equinor/echo-framework";
+import React from "react";
+import ReactDOM from "react-dom";
+import Info from "./components/info";
+PanelHandler.registerCorePanels(searchPanel, mainMenu);
+
+const Echo: React.FC = (): JSX.Element => {
+  const isAuthenticated = EchoCore.useEchoSetup({
+    leftPanel: searchPanel,
+    rightPanel: mainMenu,
+  });
+
+  return (
+    <>
+      {isAuthenticated && (
+        <EchoContent>
+          <Info />
+        </EchoContent>
+      )}
+    </>
+  );
+};
+
+if (!(window !== window.parent && !window.opener)) {
+  ReactDOM.render(<Echo />, document.getElementById("root"));
+}
---
+++ EchoClient/webpack.config.ts
@@ -0,0 +1,86 @@
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+import Dotenv from 'dotenv-webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+
+const webpackConfig = {
+    entry: ['babel-polyfill', 'react-hot-loader/patch', './src/index.tsx'],
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'v2-assets/[name].[contenthash].bundle.js',
+        chunkFilename: 'v2-assets/[name].[contenthash].chunk.js',
+
+        publicPath: '/'
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                            modules: {
+                                mode: 'local',
+                                localIdentName: 'echo-[folder]__[local]-[hash:base64:5]'
+                            }
+                        }
+                    }
+                ],
+                include: /\.module\.css$/
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+                exclude: /\.module\.css$/
+            },
+            {
+                test: /\.svg$/,
+                use: ['@svgr/webpack', 'url-loader']
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'build'),
+        compress: true,
+        port: 3000,
+        historyApiFallback: true,
+        http2: true
+        // overlay: {
+        //     warnings: true,
+        //     errors: true
+        // }
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './public/index.html')
+        }),
+        new Dotenv({
+            path: './.env'
+        })
+    ],
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        },
+        minimize: false
+    }
+};
+
+export default webpackConfig;
