197be5c86b6bac8b8aa785f5ca57a0feb1885604
Chris Skjørestad <chrs@equinor.com>
2021-05-06 08:47:02 +0200

    Exportet Toaster and bumped Echo-Core version

--- EchoFramework/src/index.ts
+++ EchoFramework/src/index.ts
@@ -1,9 +1,11 @@
 export { default as PageMenu } from './components/pageMenu/pageMenu';
 export { default as PlantSelector } from './components/plantSelector/plantSelector';
 export { default as Toast } from './components/toaster/toaster';
+export { default as Toasters } from './components/toaster/toasters';
 export { default as EchoContent } from './coreApplication/EchoContent';
 export { mainMenu, searchPanel } from './coreApplication/EchoContentPanels';
 export { default as EchoEventHandler } from './coreApplication/EchoEventHandler';
 export { themeConst } from './theme/themeConst';
 export { startup } from './utils/startup';
 import './theme/theme.css';
+
